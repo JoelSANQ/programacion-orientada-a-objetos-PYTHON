ca030fc1f2b4b608885d825b59d95ff19a2192a3
JoelSANQ <joelsanchequi@gmail.com>
2025-01-08 11:30:54 -0600

    second commit

--- Programas/poo.py
+++ Programas/poo.py
@@ -1,1 +1,20 @@
-ola 
+class personaje:
+    nombre = 'Default'
+    fuerza = 0
+    inteligencia = 0
+    defensa = 0
+    vids = 0
+    #Indicar que no se haga nada en este momento
+    pass
+mi_personaje = personaje()
+mi_personaje.nombre = "elbarto"
+mi_personaje.fuerza = 3000
+mi_personaje.inteligencia = 2
+mi_personaje.defensa = 2
+mi_personaje.vids = 2
+
+print("el nombre del personaje es ",mi_personaje.nombre)
+print("el nombre del personaje es ",mi_personaje.fuerza)
+print("el nombre del personaje es ",mi_personaje.inteligencia)
+print("el nombre del personaje es ",mi_personaje.defensa)
+print("el nombre del personaje es ",mi_personaje.vids)
