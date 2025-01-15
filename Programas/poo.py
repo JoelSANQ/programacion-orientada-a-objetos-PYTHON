ef44704f9a4ccbfc145a543856d3f1d42fe1a3ce
JoelSANQ <joelsanchequi@gmail.com>
2025-01-15 11:06:05 -0600

    simulando encapsulamiento

--- Programas/poo.py
+++ Programas/poo.py
@@ -9,60 +9,82 @@ class Personaje:
 
     # Constructor para inicializar atributos
     def __init__(self, nombre, fuerza, inteligencia, defensa, vida):
-        self.nombre = nombre
-        self.fuerza = fuerza
-        self.inteligencia = inteligencia
-        self.defensa = defensa
-        self.vida = vida
+        self.__nombre = nombre
+        self.__fuerza = fuerza
+        self.__inteligencia = inteligencia
+        self.__defensa = defensa
+        self.__vida = vida
 
     def imprimir_atributos(self):
-        print(self.nombre)
-        print("-Fuerza:", self.fuerza)
-        print("-Inteligencia:", self.inteligencia)
-        print("-defensa:", self.defensa)
-        print("-vida:", self.vida)
+        print(self.__nombre)
+        print("-Fuerza:", self.__fuerza)
+        print("-Inteligencia:", self.__inteligencia)
+        print("-defensa:", self.__defensa)
+        print("-vida:", self.__vida)
 
     def subir_nivel(self, fuerza, inteligencia, defensa):
-        self.fuerza = self.fuerza + fuerza
-        self.inteligencia = self.inteligencia + inteligencia
-        self.defensa = self.defensa + defensa
+        self.__fuerza = self.__fuerza + fuerza
+        self.__inteligencia = self.__inteligencia + inteligencia
+        self.__defensa = self.__defensa + defensa
 
     def vivo(self):
-        return self.vida > 0
+        return self.__vida > 0
 
     def muerto(self):
-        self.vida = 0
-        print(self.nombre, "ha muerto")
+        self.__vida = 0
+        print(self.__nombre, "ha muerto")
         #return self.muerto <= 0
     
     def dañar(self, enemigo):
-        return self.fuerza - enemigo.defensa
+        return self.__fuerza - enemigo.__defensa
 
     def atacar(self, enemigo):
         daño=self.dañar(enemigo)
-        enemigo.vida = enemigo.vida-daño
-        print(self.nombre, "a realizado", daño, "puntos de daño a" , enemigo.nombre)
-        print("vida de" , enemigo.nombre, "Es", enemigo.vida)
+        enemigo.__vida = enemigo.__vida-daño
+        if daño <= 0:  # Caso donde la fuerza no supera la defensa
+            print(f"{self.__nombre} no pudo atravesar la defensa de {enemigo.__nombre}.")
+        else:
+            enemigo.__vida -= daño
+            print(f"{self.__nombre} ha realizado {daño} puntos de daño a {enemigo.__nombre}.")
+            if enemigo.__vida <= 0:
+                enemigo.muerto()
+            else:
+                print(f"La vida de {enemigo.__nombre} ahora es {enemigo.__vida}.")
+    
+    def get_fuerza(self):
+            return self.__fuerza
+    
+    def set_fuerza(self, fuerza):
+        if fuerza < 0 :
+            print("error")
+        self.__fuerza = fuerza
+
+            
 
 # Creación del objeto mi_personaje
-mi_personaje = Personaje("homero simpson", 1000, 3, 70, 100,)
-mi_personaje.imprimir_atributos()
-mi_personaje.subir_nivel(10, 1, 5)
-mi_personaje.imprimir_atributos()
+mi_personaje = Personaje("homero simpson", 1000, 3, 70, 100)
+#mi_personaje.imprimir_atributos()
+#mi_personaje.subir_nivel(10, 1, 5)
+#mi_personaje.imprimir_atributos()
 mi_enemigo = Personaje("Ned flanders", 70, 30,70,100)
-mi_personaje.atacar(mi_enemigo)
-mi_enemigo.imprimir_atributos()
+#mi_personaje.fuerza
+#mi_personaje.fuerza = 0
+#mi_personaje.imprimir_atributos()
+#mi_personaje.atacar(mi_enemigo)
+#mi_enemigo.imprimir_atributos()
+mi_personaje.set_fuerza(-5)
+print(mi_personaje.set_fuerza())
 
-print(mi_personaje.dañar(mi_enemigo))
-print(mi_personaje.vivo())
-print(mi_personaje.muerto())
+#print(mi_personaje.dañar(mi_enemigo))
+#print(mi_personaje.vivo())
+#print(mi_personaje.muerto())
 
 # Sobreescritura de atributos (comentada en este caso)
-# mi_personaje.nombre = "elbarto"
-# mi_personaje.fuerza = 3000
-# mi_personaje.inteligencia = 2
-# mi_personaje.defensa = 2
-# mi_personaje.vida = 2
+# mi_personaje.__nombre = "elbarto"
+# mi_personaje.__fuerza = 3000
+# mi_personaje.__inteligencia = 2
+# mi_personaje.__defensa = 2
+# mi_personaje.__vida = 2
 
 
 
