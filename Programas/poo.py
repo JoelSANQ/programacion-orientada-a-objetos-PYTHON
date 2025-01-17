8d83f0a369ec05d553909224d7d8ef7c4321ec97
JoelSANQ <joelsanchequi@gmail.com>
2025-01-17 11:34:18 -0600

    matadero

--- Programas/poo.py
+++ Programas/poo.py
@@ -45,8 +45,72 @@ class Personaje:
         print("vida de" , enemigo.nombre, "Es", enemigo.vida)
 
 class Guerrero(Personaje):
-    pass
-tlatoani = Guerrero("Apocalipto", 50,70,30,100)
+    #sobreescribir constructor
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, espada):
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        self.espada = espada
+
+    #sobreescribir impresion
+    def imprimir_atributos(self):
+        super().imprimir_atributos()
+        print("-Espada:", self.espada)
+    def elegir_arma(self):
+        opcion= int(input("elige un arma: \n (1)Lanza de obsidiana, daño 10 (2) lanza de chaya, daño(5)\n>>>>>>>>"))
+        if opcion ==1:
+            self.espada = 10
+
+        elif opcion ==2:
+            self.espada = 8
+        else:
+            self.elegir_arma()
+
+    #sobreescribir calculo de daño
+def daño (self, enemigo):
+    return self.fuerza*self.espada - enemigo.defensa
+
+class MAGO(Personaje):
+    #sobreescribir constructor
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida, libro):
+        super().__init__(nombre, fuerza, inteligencia, defensa, vida)
+        self.libro = libro
+
+    #sobreescribir impresion
+    def imprimir_atributos(self):
+        super().imprimir_atributos()
+        print("-Espada:", self.libro)
+    def elegir_arma(self):
+        opcion= int(input("elige un arma: \n (1)hechizos de ensamblador, daño 10 (2) recetario de chaya, daño(2)\n>>>>>>>>"))
+        if opcion ==1:
+            self.libro = 10
+
+        elif opcion ==2:
+            self.libro = 8
+        else:
+            self.elegir_arma()
+
+def daño (self, enemigo):
+    return self.arma*self.arma - enemigo.defensa
+
+
+            
+
+
+michael_jackson = Personaje("michael jackson", 20,15,30,100,)
+tlatoani = Guerrero("tlatoani", 50,70,30,100,5)
+merlin = MAGO("merlin", 50,70,30,100,5)
+
+#tlatoani.elegir_arma()
+#merlin.elegir_arma
+michael_jackson.atacar(tlatoani)
+tlatoani.atacar(merlin)
+merlin.atacar(michael_jackson)
+
+
+#imprimir atributos antes de la tragedia
+tlatoani.imprimir_atributos()
+merlin.imprimir_atributos()
+michael_jackson.imprimir_atributos()
+
 
 # Creación del objeto mi_personaje
 # mi_personaje = Personaje("homero simpson", 1000, 3, 70, 100,)
